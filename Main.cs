7ede754d26a05fd6a1c83394e801702a9f1d13cb
JhonCamargo07 <jhonalexcamargo07@gmail.com>
2023-05-06 11:14:02 -0500

    Functional project

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF_TO_IMG_CONVERTER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Convirtiendo cada página del pdf en una img");
+
+            var pdfToImageConverter = new PdfToImageConverter();
+            pdfToImageConverter.ConvertPdfToImage("C:\\prueba.pdf", "C:\\Users\\jhona\\Downloads\\");
+
+            Console.WriteLine("Todas las páginas del pdf ahora son imgs y se guardaron en la ruta indicada");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Spire.Pdf;
+using Spire.Pdf.Graphics;
+using System.Drawing.Imaging;
+
+public class PdfToImageConverter
+{
+    public void ConvertPdfToImage(string pdfFilePath, string outputFilePath)
+    {
+        using (var pdfDocument = new PdfDocument())
+        {
+            pdfDocument.LoadFromFile(pdfFilePath);
+
+            for (int pageIndex = 0; pageIndex < pdfDocument.Pages.Count; pageIndex++)
+            {
+                using (var pageImage = pdfDocument.SaveAsImage(pageIndex, PdfImageType.Bitmap, 300, 300))
+                {
+                    pageImage.Save($"{outputFilePath}\\archivo_{pageIndex}.png", ImageFormat.Png);
+                }
+            }
+        }
+    }
+}
